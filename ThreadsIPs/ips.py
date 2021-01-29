373413238ad28c295277d79ae3fab7cc99ba0b60
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-01-29 11:19:08 -0300

    Update ips

---
+++ ThreadsIPs/ips.py
@@ -0,0 +1,12 @@
+import ipaddress
+
+ip1 = '192.168.0.1'
+ip2 = '192.168.0.0/24'
+
+endereco = ipaddress.ip_address(ip1)
+rede = ipaddress.ip_network(ip2, strict=False)
+
+print(endereco)
+
+for ip in rede:
+    print(rede)
